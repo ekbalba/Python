cc9bab781a8647990cfbac449e8ce9a1cec09202
ekbalba <ekbalba@outlook.com>
2019-04-15 23:13:34 -0700

    Update Palindrom_Checker.py
    added new function that removes special characters

--- Palindrome_Checker.py
+++ Palindrome_Checker.py
@@ -2,21 +2,24 @@
 # DESCRIPTION: A simple script which checks if a given phrase is a Palindrome
 # PALINDROME: A word, phrase, or sequence that reads the same backward as forward
 
-samplePhrase = "A man, a plan, a cat, a ham, a yak, a yam, a hat, a canal-Panama!"
-#givenPhrase = ""
-#phrase = ""
+sample_phrase = "A man, a plan, a cat, a ham, a yak, a yam, a hat, a canal-Panama!"
+# givenPhrase = ""
+# phrase = ""
 
-givenPhrase = input("\nPlease input a phrase:(Press ENTER to use the sample phrase) ")
+given_phrase = input("\nPlease input a phrase:(Press ENTER to use the sample phrase) ")
 
-if givenPhrase == "" or not givenPhrase.strip():
-    print("\nThe sample phrase is: {0}".format(samplePhrase))
-    phrase = samplePhrase
+def Remove_Special_Characters(phrase):
+    string = ''.join(e for e in phrase.lower() if e.isalnum())
+    return string
+
+if given_phrase == "" or not given_phrase.strip():
+    print("\nThe sample phrase is: {0}".format(sample_phrase))
+    string = Remove_Special_Characters(sample_phrase)
 else:
-    phrase = givenPhrase
-
-string = phrase.lower()
+    string = Remove_Special_Characters(given_phrase)
 
 if string == string[::-1]:
     print("\nWow!, The phrase is a Palindrome!")
 else:
     print("\nSorry, The given phrase is not a Palindrome.")
+
